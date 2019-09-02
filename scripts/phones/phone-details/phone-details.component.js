b756c5caba55b0532dfb296183d818f159ec442c
Ekaterina <yushkaaa@icloud.com>
2019-09-02 23:13:22 +0800

    changed phone-details,have some bugs

--- scripts/phones/phone-details/phone-details.component.js
+++ scripts/phones/phone-details/phone-details.component.js
@@ -45,5 +45,23 @@ export class PhoneDetailsComponent extends BaseComponent{
   </div>
         `
     }
+}
 
+let largeImg = document.querySelector('.phone');
+let smallImg = document.querySelector('.phone-thumbs');
+
+smallImg.onclick = function(e){
+  let target = e.target;
+
+  while(target != this ){
+    if(target.nodeName == 'Img'){
+      showSmall(target.src);
+      return false;
+    }
+    target = target.parentNode;
+    //console.log(target)
+  }
+  function showSmall(src) {
+    largeImg.src = src;
+  } 
 }
